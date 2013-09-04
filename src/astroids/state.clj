5c4314fd940894624763f0a167045deb55fde78a
mn <mn@ubuntubox.(none)>
2013-09-04 19:41:55 +0200

    add movements for ship

--- src/astroids/state.clj
+++ src/astroids/state.clj
@@ -3,8 +3,22 @@
 (defn world [state]
   state)
 
+(defn set-velocity [state]
+  (let [velocity (get-in state [:player :velocity])]
+    (if ((:buttons state) :space)
+      (assoc-in state [:player :velocity]
+                (mapv + [0.1 0] (:velocity (:player state))))
+      state)))
+
+(defn move-ship [state]
+  (let [velocity (get-in state [:player :velocity])
+        position (get-in state [:player :position])]
+    (assoc-in state [:player :position] (mapv + velocity position))))
+
 (defn ship [state]
-  state)
+  (-> state
+      move-ship
+      set-velocity))
 
 (defn lasers [state]
   state)
